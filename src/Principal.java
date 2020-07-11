39b8a95c4ea3988c528c0015ff42a1a711ffe355
Júlio Moura <juliomourajom@gmail.com>
2020-07-11 14:37:13 -0300

    :cool: Visualizando a primeira thread

---
+++ src/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Essa é a thread main");
+
+        Thread.sleep(30000);
+    }
+}
